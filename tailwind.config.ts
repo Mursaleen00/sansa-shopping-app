bf4729804d9836637ab5d7569173b1d6983fb872
mursaleen-zenkoders <mursaleen@zenkoders.com>
2024-12-25 02:51:48 +0500

    `update tailwind`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: { DEFAULT: '#8AC732', length: '#F1FDDF' },
+        primary: { DEFAULT: '#8AC732', light: '#F1FDDF' },
         secondary: {
           DEFAULT: '#B3B7B1',
           '100': '#DADFD8',
