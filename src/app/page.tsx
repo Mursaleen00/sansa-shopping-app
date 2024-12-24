03419e263d3daf38419faf9cd27c2103b04108e6
mursaleen-zenkoders <mursaleen@zenkoders.com>
2024-12-25 00:57:22 +0500

    `Feat`: - Setup tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className='text-primary'>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique et
       dolore at vero nostrum iste consequuntur saepe nam voluptatum corporis
       debitis tenetur illum omnis, quam, nisi ducimus ad nesciunt tempora.
