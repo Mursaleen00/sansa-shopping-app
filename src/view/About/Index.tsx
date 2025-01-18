a68da07ecdc1c2fe97cb32fcca6f36eebe8edfe1
Mursaleen00 <playessport@gmail.com>
2025-01-18 15:48:21 +0500

    `Feat`: - Remove Counter from input type number

--- src/view/About/Index.tsx
+++ src/view/About/Index.tsx
@@ -1,13 +1,9 @@
-import Details from '@/Components/user-details/your-details';
-import React from 'react';
-
 const AboutView = () => {
   return (
     <div>
       <div className='flex gap-1'>
         <div className='text-[#B3B7B1]'>Home</div>/<div>About us</div>
       </div>
-      <Details />
     </div>
   );
 };
