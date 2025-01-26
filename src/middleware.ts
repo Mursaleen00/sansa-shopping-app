aba68b06102ab17ee16b9503c1a23680d1dd478b
Mursaleen00 <playessport@gmail.com>
2025-01-26 17:44:22 +0500

    `Feat`: - update middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,27 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
   const { pathname } = req.nextUrl.clone();
 
+  console.log('Token:', token);
+  console.log('Pathname:', pathname);
+
   const publicRoutes = ['/sign-in', '/sign-up'];
   const protectedRoutes = ['/cart', '/profile'];
 
-  if (token && publicRoutes.includes(pathname))
+  // Redirect authenticated users away from sign-in/sign-up
+  if (token && publicRoutes.includes(pathname)) {
+    console.log('Redirecting authenticated user from public route to home');
     return NextResponse.redirect(new URL('/', req.url));
+  }
 
-  if (!token && protectedRoutes.includes(pathname))
+  // Redirect unauthenticated users away from protected routes
+  if (!token && protectedRoutes.includes(pathname)) {
+    console.log(
+      'Redirecting unauthenticated user from protected route to sign-in',
+    );
     return NextResponse.redirect(new URL('/sign-in', req.url));
+  }
 
+  // Allow the request to proceed if it's neither a public nor protected route
+  console.log('Allowing request to proceed');
   return NextResponse.next();
 }
